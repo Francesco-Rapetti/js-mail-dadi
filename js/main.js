b2023273c40c8a4247f78816666f0790d669b3ba
Francesco Rapetti <rapettif80@yahoo.it>
2023-10-31 18:25:10 +0100

    Implement animations

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
 const emails = ['gennaro@gmail.com', 'peppe@gmail.com', 'john@gmail.com', 'jane@gmail.com'];
 let email;
 let control = false;
+const glass = document.getElementById('glass');
+const btn = document.getElementById('throw');
+const results = document.getElementById('results');
 
 logIn();
+// glass.classList.remove('d-none');
+
 
 function logIn() {
     email = prompt('Inserisci la tua mail');
@@ -15,9 +21,21 @@ function logIn() {
 
     if (control) {
         alert('Utente verificato, ACCESSO AUTORIZZATO');
+        glass.classList.remove('d-none');
     } else {
         alert('Utente non verificato, ACCESSO NEGATO');
         logIn();
     }
+}
+
+btn.addEventListener('click', function() {
+    btn.innerHTML = 'Rilancia il dado';
+    glass.classList.add('expanded');
+    displayResults();
+})
+
+const displayResults = async () => {
+    await delay(500);
+    results.classList.add('display');
 }
 
